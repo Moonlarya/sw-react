aef2dce921924ae75b099aff40b5d009f339a6d7
Moonlarya <gum225@gmail.com>
2020-09-17 16:55:29 +0300

    Updates

---
+++ precache-manifest.69e37c3f40cb00796d095d9e8a03d1fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1215da1fc50e01a11a0910a13a161da1",
+    "url": "/sw-react/index.html"
+  },
+  {
+    "revision": "afa07fc08686b2d5b999",
+    "url": "/sw-react/static/css/main.1a0b2594.chunk.css"
+  },
+  {
+    "revision": "461e2d13a0cac926ccf4",
+    "url": "/sw-react/static/js/2.6db27c95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sw-react/static/js/2.6db27c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa07fc08686b2d5b999",
+    "url": "/sw-react/static/js/main.48c4bbb5.chunk.js"
+  },
+  {
+    "revision": "5b31381f6cb85b566ec2",
+    "url": "/sw-react/static/js/runtime-main.200a468d.js"
+  }
+]);
